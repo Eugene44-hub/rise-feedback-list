365ff3569dccda66280a16fc4170ca2b961f9496
Eugene44-hub <eugeneishado44@gmail.com>
2025-06-05 20:21:46 +0100

    hotfix: add empty state

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
 const isDev = import.meta.env.DEV;
 
-const API_URL = isDev ? "/feedback-api" : import.meta.env.VITE_API_URL;
+const API_URL = isDev ? "/feedback-api" : import.meta.env.VITE_BASE_URL;
 
 export default API_URL;
